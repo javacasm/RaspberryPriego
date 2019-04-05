1b2e6e9aee63650f21c48e78fb5c2f280a3bf348
javacasm <javacasm@gmail.com>
2019-04-05 13:49:46 +0200

    add

---
+++ codigo/python/bucle_100.py
@@ -0,0 +1,12 @@
+import time
+
+print("imprime los primeros 100 numeros")
+#  ejemplo de bucle for
+
+print("empezamos")
+
+for i in range(0,100):
+    print(i)
+    time.sleep(0.100)
+    
+print("terminamos")
